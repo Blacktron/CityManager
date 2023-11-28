d9b18997a0d513a76a76e102ebe92c66df2bbfbe
Blacktron <terrax@abv.bg>
2023-11-28 17:22:10 +0200

    Creating an endpoint to display city data including density

---
+++ configs/serverConfig.js
@@ -0,0 +1,5 @@
+const LISTENING_PORT = 3000;
+
+module.exports = {
+    listeningPort: LISTENING_PORT
+}
--- index.js
+++ index.js
@@ -1,2 +1,35 @@
+'use_strict';
+
+const fs = require('fs');
 const express = require('express');
+const serverConfig = require('./configs/serverConfig');
+
 const app = express();
+
+let cityData;
+fs.readFile('./cities.json', 'utf8', function(err, data) {
+    if (err) {
+        throw err;
+    }
+
+    cityData = JSON.parse(data);
+});
+
+
+app.get('/', (req, res) => {
+    let newCityData = cityData.map(function(city) {
+        let cityAreaKm = city.area * 1.6;
+        let populationDensity = Math.round((city.population / cityAreaKm) * 100) / 100;
+
+        return {
+            name: city.name,
+            area: city.area,
+            population: city.population,
+            density: populationDensity
+        };
+    });
+
+    res.send(newCityData);
+});
+
+app.listen(serverConfig.listeningPort);
