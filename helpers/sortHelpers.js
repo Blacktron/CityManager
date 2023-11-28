fa167e22d66b9580dc3e38daf0b849c533f22db4
Blacktron <terrax@abv.bg>
2023-11-28 22:28:22 +0200

    Adding functionality to sort by name, area, or population in ascending or descending order

---
+++ helpers/sortHelpers.js
@@ -0,0 +1,23 @@
+function sortCities(arr, propertyName, order = 'ascending') {
+    const sortedCities = arr.sort((a, b) => {
+        if (a[propertyName] < b[propertyName]) {
+            return -1;
+        }
+
+        if (a[propertyName] > b[propertyName]) {
+            return 1;
+        }
+
+        return 0;
+    });
+
+    if (order === 'descending') {
+        sortedCities.reverse();
+    }
+
+    return sortedCities;
+}
+
+module.exports = {
+    sortCities: sortCities
+}
--- index.js
+++ index.js
@@ -3,9 +3,10 @@
 const fs = require('fs');
 const express = require('express');
 const serverConfig = require('./configs/serverConfig');
+const sortHelper = require('./helpers/sortHelpers')
 
 const app = express();
-
+// TODO Remove duplicates from city data
 let cityData;
 fs.readFile('./cities.json', 'utf8', function(err, data) {
     if (err) {
@@ -15,9 +16,9 @@ fs.readFile('./cities.json', 'utf8', function(err, data) {
     cityData = JSON.parse(data);
 });
 
-
+let newCityData;
 app.get('/', (req, res) => {
-    let newCityData = cityData.map(function(city) {
+    newCityData = cityData.map(function(city) {
         let cityAreaKm = city.area * 1.6;
         let populationDensity = Math.round((city.population / cityAreaKm) * 100) / 100;
 
@@ -32,4 +33,27 @@ app.get('/', (req, res) => {
     res.send(newCityData);
 });
 
+app.get('/sort', (req, res) => {
+    let sortKey;
+    if (req.query.sortBy) {
+        sortKey = req.query.sortBy;
+    }
+
+    if (sortKey) {
+        if (req.query.order && req.query.order.toLocaleLowerCase() === 'descending') {
+            sortHelper.sortCities(newCityData, sortKey, req.query.order);
+        } else {
+            sortHelper.sortCities(newCityData, sortKey);
+        }
+    } else {
+        return newCityData;
+    }
+
+
+
+    res.send(newCityData)
+});
+
 app.listen(serverConfig.listeningPort);
+
+
